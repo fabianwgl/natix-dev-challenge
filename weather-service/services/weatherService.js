2f60d83b317ea4fbebab4c45959c4413204f0b63
Fabian General <fabian.general@autodesk.com>
2025-07-13 17:28:26 +0100

    feat: adds cache to avoid recalculating data (.3 context and scope)

--- weather-service/services/weatherService.js
+++ weather-service/services/weatherService.js
@@ -3,8 +3,16 @@ const weatherConditions = ['Clear', 'Cloudy', 'Rain', 'Fog', 'Windy', 'Snow'];
 // Track all requests globally
 let requestTimestamps = [];
 
+// Cache to store weather data and avoid recalculating
+const cache = {};
+// Cache duration (10 minutes)
+const CACHE_TTL_MS = 10 * 60 * 1000;
+
 /**
- * 
+ * Function that calculates and returns mocked weather data for a given city.
+ * It simulates a weather service by generating random weather data.
+ * It also implements a rate limit of 100 requests per hour and caches the results so that
+ * repeated requests for the same city within 10 minutes return the cached data.
  * @param {string} city 
  * @returns mocked weather data for the specified city
  */
@@ -20,6 +28,15 @@ function mockWeatherService(city) { // parameter is not used in this mock
     };
   }
 
+    // 2. Check cache for city
+  if (cache[city]) {
+    const cached = cache[city];
+    // If cached data still fresh, return it
+    if (now - cached.timestamp < CACHE_TTL_MS) {
+      return { result: cached.data};
+    }
+  }
+
   // Register the request
   requestTimestamps.push(now);
 
@@ -29,6 +46,12 @@ function mockWeatherService(city) { // parameter is not used in this mock
     temperature: `${Math.floor(14 + Math.random() * 10)}°C`,
     condition: weatherConditions[Math.floor(Math.random() * weatherConditions.length)],
   }));
+
+    // 5. Cache the new data
+  cache[city] = {
+    timestamp: now,
+    data: result,
+  };
 
   return { result };
 }
